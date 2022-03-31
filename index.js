1d4d15c2406e50954bc2ebfce09410025e8b2943
Singgih Pratama <78544446+mastama@users.noreply.github.com>
2022-03-31 14:20:36 +0700

    Set up has finised, homepageBeforeLogin, contact

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 8000;
+
+// app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+// static files
+app.use(express.static("public"));
+app.use("/", express.static("/public"));
+
+// set View's
+app.set('views', './views')
+app.set('view engine', 'ejs');
+
+// routes
+app.get('/', (req, res) => {
+    res.render("pages/hpBeforeLogin/index");
+})
+
+// contact routes
+app.get('/contact', (req, res) => {
+    res.render("pages/contactUs/index");
+})
+
+// afterLogin routes
+app.get('/afterlogin', (req, res) => {
+    res.render("pages/hpAfterLogin/index");
+})
+
+// classes routes
+app.get('/classes', (req, res) => {
+    res.render("pages/classes/index");
+})
+
+// login
+app.get('/login', (req, res) => {
+    res.render("pages/login/index");
+})
+
+app.listen(port, () => {
+    console.log(`Server is listening on ${port}`);
+})
