328e2724d845ff1f45a74610b7da91814715199a
Fachrul <fachrulfatahillah@gmail.com>
2022-04-03 14:57:41 +0700

    Sidebar Menu

--- index.js
+++ index.js
@@ -1,51 +1,53 @@
 const express = require('express');
 const app = express();
-require("dotenv").config();
+require('dotenv').config();
 const port = process.env.PORT || 8000;
 
 // app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 // static files
-app.use(express.static("public"));
-app.use("/", express.static("/public"));
+app.use(express.static('public'));
+app.use('/', express.static('/public'));
 
 // set View's
-app.set('views', './views')
+app.set('views', './views');
 app.set('view engine', 'ejs');
 
 // routes
 app.get('/', (req, res) => {
-    res.render("pages/hpBeforeLogin/index");
-})
+  res.render('pages/hpBeforeLogin/index');
+});
 
 // contact routes
 app.get('/contact', (req, res) => {
-    res.render("pages/contactUs/index");
-})
+  res.render('pages/contactUs/index');
+});
 
-// afterLogin routes
-app.get('/afterlogin', (req, res) => {
-    res.render("pages/hpAfterLogin/index");
-})
+// home routes
+app.get('/home', (req, res) => {
+  res.render('pages/home/index');
+});
 
 // classes routes
 app.get('/classes', (req, res) => {
-    res.render("pages/classes/index");
-})
+  res.render('pages/classes/index');
+});
 
 // login
 app.get('/login', (req, res) => {
-    res.render("pages/login/index");
-})
+  res.render('pages/login/index');
+});
 
 // register
 app.get('/register', (req, res) => {
-    res.render("pages/register/index");
-})
+  res.render('pages/register/index');
+});
 
 app.listen(port, () => {
-    console.log(`Server is listening on ${port}`);
-})
+  console.log(`Server is listening on ${port}`);
+});
