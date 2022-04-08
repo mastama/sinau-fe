c309737524b932363aa4bc3fc14263d285cbcb62
Fachrul <fachrulfatahillah@gmail.com>
2022-04-09 01:31:01 +0700

    Add Pages Teacher

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ require('dotenv').config();
 const port = process.env.PORT || 8000;
 
 // app.use(cors());
+app.use(express.static('views/assets'));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 app.use(express.json());
 app.use(
   express.urlencoded({
@@ -35,7 +38,7 @@ app.get('/home', (req, res) => {
 
 // presensi guru routes
 app.get('/presensi-guru', (req, res) => {
-  res.render('pages/presensi/presensiguru');
+  res.render('pages/presensi/index');
 });
 
 // presensi table guru routes
@@ -44,6 +47,10 @@ app.get('/presensi-table', (req, res) => {
 });
 
 // classes routes
+app.get('/teacher', (req, res) => {
+  res.render('pages/teacher/index');
+});
+
 app.get('/classes', (req, res) => {
   res.render('pages/classes/index');
 });
